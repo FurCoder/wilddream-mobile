2954fc332d6a3513e73c1183ff7990f71e04aaca
wangziyang_ark <wangziyang_ark@douban.com>
2019-05-31 14:52:33 +0800

    鸽子转念一想,给还是加一下页面缓存吧嘤嘤嘤

--- app/App.js
+++ app/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom'
+import NotLiveRoute from "react-live-route"
 import moment from 'moment'
-
 import HomePage from '@page/HomePage'
 import LoginPage from '@page/LoginPage'
 import ArtDetail from '@page/ArtDetailPage'
@@ -11,12 +11,13 @@ import UserPage from '@page/UserPage'
 import 'antd-mobile/dist/antd-mobile.css'
 import '@style/index.styl'
 
+const LiveRoute = withRouter(NotLiveRoute)
 moment.locale('zh-cn')
 
 render(
   <Router>
     <div className='full-size'>
-      <Route exact path="/" component={HomePage} />
+      <LiveRoute exact alwaysLive path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
       <Route path="/art-detail/:userid/:artworkid" component={ArtDetail} />
       <Route path="/user/:userid" component={UserPage} />
