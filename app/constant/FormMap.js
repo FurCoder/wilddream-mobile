0aa4bd1584b6c92b0889de36e12216e26e02518b
melchior voidwolf <kmno4k2mno4@gmail.com>
2019-08-16 00:16:17 +0800

    好像忘了提代码了

---
+++ app/constant/FormMap.js
@@ -0,0 +1,18 @@
+export const ArtLevelMap = [
+  {en: 'hobbyist', cn: '业余'},
+  {en: 'student', cn: '学生'},
+  {en: 'professional', cn: '专业'},
+]
+
+export const ArtSpeciality = [
+  {en: 'digital_art', cn: '数字绘画'},
+  {en: 'raditional_art', cn: '传统绘画'},
+  {en: 'design', cn: '设计'},
+  {en: 'film_and_anime', cn: '视频和动画'},
+  {en: 'literature', cn: '文学'},
+  {en: 'photography', cn: '摄影'},
+  {en: 'artisan', cn: '手工制作'},
+  {en: 'fursuiter', cn: '兽装制作和表演'},
+  {en: 'other', cn: '其他'},
+]
+
